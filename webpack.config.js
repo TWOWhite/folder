006129f26b7819920fe173a91bf0dc191edf3a83
libotao <libotao23@outlook.com>
2017-12-11 16:53:17 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: {
+        folder: './src/index.js'
+    },
+    output:{
+        filename:'./dist/folder.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+            }
+        ]
+    }
+}
